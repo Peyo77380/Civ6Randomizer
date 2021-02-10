10170cd9e0e32ef615f67b66776729c174a7246d
Peyo77380 <pierre.guichard77@gmail.com>
2021-02-10 18:14:31 +0100

    create new user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -15,6 +15,14 @@ class UserFixtures extends Fixture
         $user->setPassword('$2y$13$.swN8t0O.zQo3jJ9VTYIxeX9ALXIB6CT/4tand1jJKUckNmp9BBke');
         $user->setLocale('ru');
         $manager->persist($user);
+
+        $user2 = new User();
+        $user2->setUsername('Peyo');
+        $user2->setPassword('$2y$13$FbIayaczYNJ5o.86VewU3uJ1l..wfDpOuZ.uFD6Y2qhw5IJ0W5Gea');
+        $user2->setLocale('fr');
+        $manager->persist($user2);
         $manager->flush();
+
+
     }
 }
