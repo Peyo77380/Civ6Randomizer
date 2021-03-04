5dc4ff86afb44af5b9a8e387237bd4dcba9d7cda
Peyo77380 <pierre.guichard77@gmail.com>
2021-03-04 16:39:47 +0100

    delete typo

--- src/DataFixtures/LeaderFixtures.php
+++ src/DataFixtures/LeaderFixtures.php
@@ -83,7 +83,7 @@ class LeaderFixtures extends Fixture implements DependentFixtureInterface
         ];
         $ref[] = [
           'iso' => 'FR', 
-          'ref' => $this->getReference(LanguageFixtures::LANG_FR_REgitFERENCE)
+          'ref' => $this->getReference(LanguageFixtures::LANG_FR_REFERENCE)
         ];
         
         foreach (LeaderFixtures::LEADERS as $head) {
